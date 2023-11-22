01a1ef21bdf080ca343c8ab5e7b43a93db499bf5
Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>
2023-11-22 19:59:51 -0300

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,7 +3,7 @@
 /**
  * A serverless function handler for the '/api' route, for use with Netlify.
  *
- * See:
+ * See: 
  *  - https://docs.netlify.com/functions/overview/
  */
 exports.handler = require('../src/serverless').handler
